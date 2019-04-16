d14e7a04370652182619cd29f8739a92cb560220
Shipman <thomas.shipman@hotmail.fr>
2019-04-16 11:36:02 +0200

    fix(login.dart):architecture bloc good

--- flutter_app/lib/bloc/login_bloc.dart
+++ flutter_app/lib/bloc/login_bloc.dart
@@ -6,43 +6,29 @@ class LoginBloc {
   final _repository = Repository();
   String _email;
   String _password;
-  final emailFieldController = TextEditingController();
-  final passFieldController = TextEditingController();
 
-  LoginBloc() {
-    _email = emailFieldController.text;
-    _password = passFieldController.text;
-  }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    // Detache le controller quand tout est fini
-    emailFieldController.dispose();
-    passFieldController.dispose();
-  }
-
-  Future<int> submit() {
-    if(validateFields()) {
-      return _repository.authenticateUser(_email, _password);
+  Future<int> submit(String email, String password) {
+    if(validateFields(email, password)) {
+      return _repository.authenticateUser(email, password);
     }
     return null;
   }
 
-  Future<void> registerUser() {
-    if(validateFields()) {
-      return _repository.registerUser(_email, _password);
+  Future<void> registerUser(String email, String password) {
+    if(validateFields(email, password)) {
+      return _repository.registerUser(email, password);
     }
     return null;
   }
 
-  bool validateFields() {
-    if (emailFieldController.text != null &&
-        emailFieldController.text.isNotEmpty &&
-        passFieldController.text != null &&
-        passFieldController.text.isNotEmpty &&
-        emailFieldController.text.contains('@') &&
-        passFieldController.text.length > 3) {
+  bool validateFields(String email, String password) {
+    if (email != null &&
+        email.isNotEmpty &&
+        password != null &&
+        password.isNotEmpty &&
+        email.contains('@') &&
+        password.length > 3) {
       return true;
     } else {
       return false;
