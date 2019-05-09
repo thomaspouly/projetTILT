07c767ca17e5c4915eea6040a5e953bfbd2967b5
Thomas <tpouly@live.fr>
2019-05-09 09:13:02 +0200

    feat(staggeredView.dart): now we can flip the tiles and develop  them

--- flutter_app/lib/screen/customs/staggeredView.dart
+++ flutter_app/lib/screen/customs/staggeredView.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app/bloc/counter_bloc.dart';
 import 'package:flutter_app/models/Categorie.dart';
-import 'package:flutter_app/screen/home/tilesDetail.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:intl/intl.dart';
-
+import 'package:flutter_app/screen/home/home.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:page_transition/page_transition.dart';
 
 class StaggeredView extends StatefulWidget {
@@ -16,116 +17,248 @@ class StaggeredView extends StatefulWidget {
   final int id;
   final String description;
   final Categorie categorie;
- final DateTime date;
- final CounterBloc bloc;
-  StaggeredView(this.iconData, this.title,  this.id,
-      this.description, this.categorie,this.date,this.bloc);
+  final DateTime date;
+  final CounterBloc bloc;
+  final Function(int, bool) notifyParent;
+  final int tileId;
+  StaggeredView(this.iconData, this.title, this.id, this.description,
+      this.categorie, this.date, this.bloc, this.notifyParent, this.tileId);
 
   @override
   State<StatefulWidget> createState() => new _StaggeredViewState();
 }
 
-class _StaggeredViewState extends State<StaggeredView> {
+class _StaggeredViewState extends State<StaggeredView>
+    with SingleTickerProviderStateMixin {
   String counter;
- 
+
+  double widthScreen;
 
   @override
-   void setState(fn) {
-    if(mounted){
+  void setState(fn) {
+    if (mounted) {
       super.setState(fn);
     }
   }
 
-
+  bool developemment = false;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
- final formatter =
-                  new NumberFormat("###,###,###,###,###,###,###,###");
-  
- widget.bloc.actualise(widget.date);
 
-  counter = formatter.format( widget.bloc.counter.toInt());
+    final formatter = new NumberFormat("###,###,###,###,###,###,###,###");
+
+    widget.bloc.actualise(widget.date);
+
+    counter = formatter.format(widget.bloc.counter.toInt());
+    counter = counter.replaceAll(",", ".");
     Timer.periodic(
         Duration(seconds: 1),
         (Timer t) => setState(() {
-      
- widget.bloc.actualise(widget.date);
-             
-              counter = formatter.format( widget.bloc.counter.toInt());
+              widget.bloc.actualise(widget.date);
+
+              counter = formatter.format(widget.bloc.counter.toInt());
+              counter = counter.replaceAll(",", ".");
             }));
-  
-    
+  }
+
+  Widget back(bool b) {
+    switch (b) {
+      case false:
+         return Card(
+          color: widget.categorie.color,
+          child:  Stack(
+              
+              children: <Widget>[ Column(children: <Widget>[
+            Stack(
+              
+              children: <Widget>[
+ Align(alignment: Alignment.bottomCenter,child:Opacity(child:Icon(widget.categorie.logo,color: widget.categorie.colorLogo,size: widthScreen/3,),opacity: 0.5,)) ,
+                Container(
+                    
+                  padding: EdgeInsets.only(left: widthScreen/20,right:  widthScreen/20),
+                  child: Column(children: <Widget>[
+                    AutoSizeText(
+                
+                      counter,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: widthScreen / 10,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Monofonto-Regular"),
+                      maxLines: 1,
+                       
+
+                    ),
+                   Text(
+                     
+                      widget.description,
+                      style: TextStyle(
+                          color: widget.categorie.colorLogo, fontSize: widthScreen/28),
+                         overflow: TextOverflow.ellipsis ,
+                         maxLines: 4,
+
+                    ) 
+                  
+                  ]),
+                ),
+              ],
+            ),
+
+            
+           
+          ]),
+          Align(
+                child: FlatButton(
+                  child: Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.white,
+                    size: widthScreen / 20,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      developemment = true;
+                      widget.notifyParent(widget.tileId, false);
+                    });
+                  },
+                ),
+                alignment: Alignment.bottomRight), 
+          ])
+          
+        );
+        break;
+      case true:
+        return Card(
+          color: widget.categorie.color,
+          child:  Stack(
+              
+              children: <Widget>[ Column(children: <Widget>[
+            Stack(
+              
+              children: <Widget>[
+ Align(alignment: Alignment.bottomCenter,child:Opacity(child:Icon(widget.categorie.logo,color: widget.categorie.colorLogo,size: widthScreen/3,),opacity: 0.5,)) ,
+                Container(
+                    
+                  padding: EdgeInsets.only(left: widthScreen/20,right:  widthScreen/20),
+                  child: Column(children: <Widget>[
+                    Text(
+                      counter,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: widthScreen / 10,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Monofonto-Regular"),
+                      maxLines: 1,
+                    ),
+                    Text(
+                      widget.description,
+                      style: TextStyle(
+                          color: widget.categorie.colorLogo, fontSize: widthScreen/28),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+
+             Container(
+              
+                  padding: EdgeInsets.only(left: widthScreen/20,right:  widthScreen/20,top:widthScreen/15),
+                  child: Column(children: <Widget>[
+                    Text("Conseils:\n",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: widthScreen/22)),
+            AutoSizeText(
+              widget.categorie.conseils,
+              style: TextStyle(color: widget.categorie.colorLogo, fontSize: widthScreen/28),
+            ),
+                  ]),
+                ),
+          
+           
+          ]),
+          Align(
+                child: FlatButton(
+                  child: Icon(
+                    Icons.arrow_drop_up,
+                    color: Colors.white,
+                    size: widthScreen / 20,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      developemment = false;
+                      widget.notifyParent(widget.tileId, true);
+                    });
+                  },
+                ),
+                alignment: Alignment.bottomRight), 
+          ])
+          
+        );
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-   double widthScreen = MediaQuery.of(context).size.width;
-
-
+    widthScreen = MediaQuery.of(context).size.width;
     return new InkWell(
-        onTap: () {
-          print(widget.id);
-
-          Navigator.push(
-              context,
-              PageTransition(
-                  duration: Duration(milliseconds: 500),
-                  type: null,
-                  child: TileDetail(widget.id, widget.iconData, widget.title,
-                      widget.description, widget.categorie)));
-        },
-        child: Container(padding:EdgeInsets.only(left:2,right: 2),child:Card(
-          
-            elevation: 3,
-            color: widget.categorie.color,
-            child: new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Hero(
-                    tag: "icon_${widget.id}",
-                    child: Icon(
-                      widget.categorie.logo,
-                      color: widget.categorie.colorLogo,
-                      size: 60,
+        child: Container(
+            padding: EdgeInsets.only(left: 2, right: 2),
+            child: FlipCard(
+                direction: FlipDirection.VERTICAL, // default
+                front: Card(
+                    elevation: 3,
+                    color: widget.categorie.color,
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Hero(
+                            tag: "icon_${widget.id}",
+                            child: Icon(
+                              widget.categorie.logo,
+                              color: widget.categorie.colorLogo,
+                              size: widthScreen/7,
+                            )),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Container(
+                              width: widthScreen / 3 * 2,
+                              child: AutoSizeText(
+                                widget.title,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: widthScreen / 17,
+                                    color: widget.categorie.colorLogo,
+                                    fontWeight: FontWeight.bold),
+                                minFontSize: 10.0,
+                                stepGranularity: 10.0,
+                                maxLines: 3,
+                              ),
+                            ),
+                            Container(
+                              width: widthScreen / 3 * 2,
+                              child: Text(
+                                counter,
+                                textAlign: TextAlign.end,
+                                style: TextStyle(
+                                    fontSize: widthScreen / 10,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: "Monofonto-Regular"),
+                                maxLines: 1,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
                     )),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Container(
-                      width: widthScreen / 3 * 2,
-                      child: AutoSizeText(
-                        widget.title,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: widthScreen/17,
-                            color: widget.categorie.colorLogo,
-                            fontWeight: FontWeight.bold),
-                        minFontSize: 10.0,
-                        stepGranularity: 10.0,
-                        maxLines: 3,
-                      ),
-                    ),
-                    Container(
-                      width: widthScreen / 3 * 2,
-                      child: AutoSizeText(
-                       counter,
-                        textAlign: TextAlign.end,
-                        style: TextStyle(
-                            fontSize: widthScreen / 10,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Monofonto-Regular"),
-                        minFontSize: 7.0,
-                        stepGranularity: 10.0,
-                        maxLines: 1,
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ))));
+                back: back(developemment))));
   }
 }
