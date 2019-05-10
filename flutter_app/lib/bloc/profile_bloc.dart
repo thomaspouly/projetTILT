d3cd91b3d8e59b5aed11e99f17c17740b8957341
Shipman <thomas.shipman@hotmail.fr>
2019-05-10 11:33:30 +0200

    feat(profil.dart): refractor name var

--- flutter_app/lib/bloc/profile_bloc.dart
+++ flutter_app/lib/bloc/profile_bloc.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter_app/models/User.dart';
 import 'package:flutter_app/services/Repository.dart';
 
-class ProfilBloc {
+class ProfileBloc {
   final _repository = Repository();
 
   Future<User> getUserById(String id) {
